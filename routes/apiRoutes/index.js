0f1580b41ddf2a7a002e2a8252f10e1af8d75418
sramk87 <sramkissoon87@gmail.com>
2022-11-29 22:14:41 -0500

    api routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { addNote, deleteNote } = require("../../lib/notes");
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+  });
+
+  router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    if (!addNote(req.body)) {
+      res.status(400);
+    } else {
+      const newNote = addNote(req.body, notes);
+      res.json(newNote);
+    }
+  });
+
+  router.delete("/notes/:id" , (req, res) => {
+    const params = req.params.id
+    deleteNote(params, notes);
+    res.redirect('');
+  });
+
+  module.exports = router;
