d9f0a6b1c0aa27f061f1d0e2e2b489291e9278e2
sramk87 <sramkissoon87@gmail.com>
2022-11-29 22:06:49 -0500

    html routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.get('/notes', (req, res) => {
+    let data = notes;
+    res.json(data);
+  });
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
